57d46007b212e174d9616fc53f7427673410104a
Andreas Schubert <andreas.schubert@die-einrichtung.org>
2020-11-11 12:40:41 +0100

    added missing libraries

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-echo "**** install openvpn ****"
+echo "**** install openvpn and required libs ****"
 chmod 777 /root-layer
 apt-get update
 apt-get install -y binutils xz-utils
-apt-get download openvpn
-ar p /root-layer/openvpn*.deb data.tar.xz | tar xJv --strip-components=1 -f -
-rm openvpn*.deb
+for package in openvpn liblzo2-2 multiarch-support libpkcs11-helper1
+do
+    apt-get download $package
+    ar p /root-layer/${package}*.deb data.tar.xz | tar xJv --strip-components=1 -f -
+    rm ${package}*.deb
+done
 rm /tmp/install.sh
