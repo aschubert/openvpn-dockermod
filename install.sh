4a29bb8554276efb51ee747ae387529eaca76e62
Andreas Schubert <andreas.schubert@die-einrichtung.org>
2020-11-11 12:11:51 +0100

    fixed unpacking of openvpn

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ chmod 777 /root-layer
 apt-get update
 apt-get install -y binutils xz-utils
 apt-get download openvpn
-ar p /root-layer/openvpn*.deb data.tar.xz | tar xJv --strip-components=2 -f -
+ar p /root-layer/openvpn*.deb data.tar.xz | tar xJv --strip-components=1 -f -
 rm openvpn*.deb
 rm /tmp/install.sh
