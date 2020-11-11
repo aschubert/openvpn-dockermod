f830421507ea0fb073c58272b47204308b34f56f
Andreas Schubert <andreas.schubert@die-einrichtung.org>
2020-11-11 14:12:53 +0100

    added missing ip binary and dependencies

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "**** install openvpn and required libs ****"
 chmod 777 /root-layer
 apt-get update
 apt-get install -y binutils xz-utils
-for package in openvpn liblzo2-2 multiarch-support libpkcs11-helper1
+for package in openvpn liblzo2-2 multiarch-support libpkcs11-helper1 iproute2 libatm1 libelf1 libmnl0 libxtables12
 do
     apt-get download $package
     ar p /root-layer/${package}*.deb data.tar.xz | tar xJv --strip-components=1 -f -
